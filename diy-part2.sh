3fada1ed0ea5135a61b8edaea6543e61aeed225e
baiyunxue <84278744+baiyunxue@users.noreply.github.com>
2024-10-24 15:36:27 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,10 +14,10 @@
 sed -i 's/192.168.1.1/192.168.10.1/g' openwrt/package/base-files/files/bin/config_generate
 
 # 默认开启 WiFi
-sed -i 's/disabled=1/disabled=0/g' openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/disabled=1/disabled=0/g' openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 自定义 SSID
-sed -i 's/ssid=OpenWrt/ssid=immortalwrt/g' openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/ssid=OpenWrt/ssid=immortalwrt/g' openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 自定义主题
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile

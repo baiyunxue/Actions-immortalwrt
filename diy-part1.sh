36419eb47494694ce7044adbcfc4df8fa6ee7138
baiyunxue <84278744+baiyunxue@users.noreply.github.com>
2024-10-24 14:39:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#=============================================================
+# Copyright (c) 2021 baiyunxue
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
+# https://github.com/baiyunxue/Compile-LEDE
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (Before Update feeds)
+#=============================================================
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default

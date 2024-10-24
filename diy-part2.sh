8a106006852cb219e0172909cc6b5b1dd27e9f56
baiyunxue <84278744+baiyunxue@users.noreply.github.com>
2024-10-24 14:18:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
-# Description: OpenWrt DIY script part 2 (After Update feeds)
-#
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#=============================================================
+# Copyright (c) 2021 baiyunxue
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
 #
+# https://github.com/baiyunxue/Compile-LEDE
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (Before Update feeds)
+#=============================================================
 
-# Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 自定义 IP地址
+sed -i 's/192.168.1.1/192.168.10.1/g' openwrt/package/base-files/files/bin/config_generate
 
-# Modify default theme
+# 默认开启 WiFi
+sed -i 's/disabled=1/disabled=0/g' openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 自定义 SSID
+sed -i 's/ssid=OpenWrt/ssid=immortalwrt/g' openwrt/package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# 自定义主题
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # Modify hostname

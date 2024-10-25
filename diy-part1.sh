7c8477d558f50674d2fd545b370b50895961b3a3
baiyunxue <84278744+baiyunxue@users.noreply.github.com>
2024-10-25 23:17:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source{自定义app} 
 git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
 git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
-sed -i '$a src-git kiddin9 https://dl.openwrt.ai/packages-23.05/aarch64_cortex-a53/kiddin9.git' feeds.conf.default
+git clone https://dl.openwrt.ai/packages-23.05/aarch64_cortex-a53/kiddin9 filse/kiddin9
 # istore
 sed -i '$a src-git store https://github.com/linkease/istore.git;main' feeds.conf.default
 # argon, etc.

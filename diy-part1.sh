90a74ce59d54180613daac3273188d75b01a41a6
baiyunxue <84278744+baiyunxue@users.noreply.github.com>
2024-10-25 21:12:02 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,9 +19,9 @@
 #src-git-full routing https://git.openwrt.org/feed/routing.git;openwrt-22.03
 #src-git-full telephony https://git.openwrt.org/feed/telephony.git;openwrt-22.03
 # istore
-#src-git store https://github.com/linkease/istore.git;main
+src-git store https://github.com/linkease/istore.git;main
 # argon, etc.
-#src-git third https://github.com/jjm2473/openwrt-third.git;main
+src-git third https://github.com/jjm2473/openwrt-third.git;main
 
 #sed -i '$a src-git store https://istore.linkease.com/repo/all/store.git' feeds.conf.default
 #svn co https://github.com/animegasan/luci-app-quickstart package/files/luci-app-quickstart

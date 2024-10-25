d6684e4204a42e1c6d7d9809c58a42d0fc5cafc4
baiyunxue <84278744+baiyunxue@users.noreply.github.com>
2024-10-25 21:36:43 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,14 +14,12 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source{自定义app} 
-#src-git-full packages https://github.com/jjm2473/packages.git;istoreos-22.03
-#src-git-full luci https://github.com/jjm2473/luci.git;istoreos-22.03
-#src-git-full routing https://git.openwrt.org/feed/routing.git;openwrt-22.03
-#src-git-full telephony https://git.openwrt.org/feed/telephony.git;openwrt-22.03
+ git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
+git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
 # istore
-echo 'src-git store https://github.com/linkease/istore.git;main' >> feeds.conf.default
+sed -i '$a src-git store https://github.com/linkease/istore.git;main' feeds.conf.default
 # argon, etc.
-echo 'src-git third https://github.com/jjm2473/openwrt-third.git;main' >> feeds.conf.default
+sed -i '$a src-git third https://github.com/jjm2473/openwrt-third.git;main' feeds.conf.default
 
 #sed -i '$a src-git store https://istore.linkease.com/repo/all/store.git' feeds.conf.default
 #svn co https://github.com/animegasan/luci-app-quickstart package/files/luci-app-quickstart

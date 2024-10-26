a9b7a063c393c4a00797905bc55b70a127f92286
baiyunxue <84278744+baiyunxue@users.noreply.github.com>
2024-10-26 14:00:26 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,13 +14,13 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source{自定义app} 
-git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
-git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
-git clone https://dl.openwrt.ai/packages-23.05/aarch64_cortex-a53/kiddin9 filse/kiddin9
+#git clone https://github.com/lisaac/luci-lib-docker package/luci-lib-docker
+#git clone https://github.com/lisaac/luci-app-dockerman package/luci-app-dockerman
+#git clone https://dl.openwrt.ai/packages-23.05/aarch64_cortex-a53/kiddin9 filse/kiddin9
 # istore
-sed -i '$a src-git store https://github.com/linkease/istore.git;main' feeds.conf.default
+#sed -i '$a src-git store https://github.com/linkease/istore.git;main' feeds.conf.default
 # argon, etc.
-sed -i '$a src-git third https://github.com/jjm2473/openwrt-third.git;main' feeds.conf.default
+#sed -i '$a src-git third https://github.com/jjm2473/openwrt-third.git;main' feeds.conf.default
 
 #sed -i '$a src-git store https://istore.linkease.com/repo/all/store.git' feeds.conf.default
 #svn co https://github.com/animegasan/luci-app-quickstart package/files/luci-app-quickstart
